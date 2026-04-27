a9fe354b2f0be1e1bcf384b74ceee75288494177
温暖 <lillianwhayneso60@gmail.com>
2026-04-27 11:14:05 +0800

    chore: initial commit with project structure

---
+++ src/main/java/com/xuenai/medical/MedicalCareApplication.java
@@ -0,0 +1,13 @@
+package com.xuenai.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuenai/medical_care/MedicalCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuenai.medical_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
